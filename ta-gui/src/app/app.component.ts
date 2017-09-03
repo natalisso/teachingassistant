db040d4d3972041030fe2b24d1879a6aa4b9710e
pauloborba <phmb@cin.ufpe.br>
2017-09-03 18:23:24 -0300

    Add evitar cadastro de mais de um aluno com o mesmo cpf

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -15,8 +15,11 @@ export class AppComponent {
    alunos: Aluno[] = [];
 
    gravar(a: Aluno): void {
-     this.alunoService.gravar(a);
-     this.alunos.push(a);
-     this.aluno = {nome: "", cpf: "", email: "", login_github: ""};
+     if (this.alunoService.gravar(a)) {
+       this.alunos.push(a);
+       this.aluno = {nome: "", cpf: "", email: "", login_github: ""};
+     } else {
+       this.aluno.cpf = "";
+     }
   }
 }
