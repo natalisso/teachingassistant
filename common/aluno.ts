5e5b7b09e943f8068d48e329c315b7601cfcb2c0
natalisso <nss2@cin.ufpe.br>
2021-03-31 13:01:30 -0300

    Add login github to students parameters

--- common/aluno.ts
+++ common/aluno.ts
@@ -2,6 +2,7 @@ export class Aluno {
   nome: string;
   cpf: string;
   email: string;
+  login_github: string;
   metas: Map<string,string>;
 
   constructor() {
@@ -12,6 +13,7 @@ export class Aluno {
     this.nome = "";
     this.cpf = "";
     this.email = "";
+    this.login_github = "";
     this.metas = new Map<string,string>();
   }
 
@@ -25,6 +27,7 @@ export class Aluno {
     this.nome = from.nome;
     this.cpf = from.cpf;
     this.email = from.email;
+    this.login_github = from.login_github;
     this.copyMetasFrom(from.metas);
   }
 
