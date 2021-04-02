a7da21e1916adc296b6e8e1308ed227414a3b7c2
natalisso <nss2@cin.ufpe.br>
2021-04-02 12:20:14 -0300

    Add new unit tests

--- ta-server/spec/cadastrodealunos.spec.ts
+++ ta-server/spec/cadastrodealunos.spec.ts
@@ -4,11 +4,20 @@ import { Aluno } from '../../common/aluno';
 describe("O cadastro de alunos", () => {
   var cadastro: CadastroDeAlunos;
 
-  function cadastrarAluno(nome:string, cpf:string) {
+  function cadastrarAluno(nome:string, cpf:string, email?:string) {
     var aluno: Aluno = new Aluno();
     aluno.nome = nome;
     aluno.cpf = cpf;
+    if (email != void 0) {aluno.email = email}
     cadastro.cadastrar(aluno);
+  }
+
+  function atualizarAluno(nome:string, cpf:string, email?:string) {
+    var aluno: Aluno = new Aluno();
+    aluno.nome = nome;
+    aluno.cpf = cpf;
+    if (email != void 0) {aluno.email = email}
+    cadastro.atualizar(aluno);
   }
 
   function expectSoUmAluno() {
@@ -39,6 +48,47 @@ describe("O cadastro de alunos", () => {
 
     var aluno = expectSoUmAluno();
     expect(aluno.nome).toBe("Mariana");
+  })
+
+  it("atualiza aluno cadastrado", () => {
+    cadastrarAluno("Natália","684");
+
+    var aluno = expectSoUmAluno();
+    expect(aluno.nome).toBe("Natália");
+    expect(aluno.cpf).toBe("684");
+    expect(aluno.email).toBe("");
+    expect(aluno.metas.size).toBe(0);
+
+    atualizarAluno("Luisa", "684")
+
+    var aluno = expectSoUmAluno();
+    expect(aluno.nome).toBe("Luisa");
+    expect(aluno.cpf).toBe("684");
+    expect(aluno.email).toBe("");
+    expect(aluno.metas.size).toBe(0);
+  })
+
+  it("aceita alunos com CPFs diferentes", () => {
+    cadastrarAluno("Juliana","685");
+    cadastrarAluno("André","686");
+
+    expect(cadastro.getAlunos().length).toBe(2);
+
+    var aluno = cadastro.getAlunos()[0];
+    expect(aluno.nome).toBe("Juliana");
+
+    var aluno = cadastro.getAlunos()[1];
+    expect(aluno.nome).toBe("André");
+  })
+
+  it("cadastra alunos com email corretamente", () => {
+    cadastrarAluno("Victor","687", "vss@cin.ufpe.br");
+
+    var aluno = expectSoUmAluno();
+    expect(aluno.nome).toBe("Victor");
+    expect(aluno.cpf).toBe("687");
+    expect(aluno.email).toBe("vss@cin.ufpe.br");
+    expect(aluno.metas.size).toBe(0);
   })
 
 })
